4de7248297342cd57f41711cb3a5c46d04020354
Akshay Golash <akshay.golash95@gmail.com>
2016-10-07 01:13:50 -0500

    Changes in code

---
+++ app/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Col } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import ReactForms from './app.jsx';
+
+ReactDom.render(<ReactForms/>,document.getElementById('app'));
