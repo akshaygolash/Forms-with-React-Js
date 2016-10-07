c274adc41ba58081751991cf2d8669c7e279c198
Akshay Golash <akshay.golash95@gmail.com>
2016-10-07 01:12:14 -0500

    old code

--- app/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { Col } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import ReactForms from './app.jsx';
-
-ReactDom.render(<ReactForms/>,document.getElementById('app'));
